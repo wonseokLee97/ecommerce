25d503c17fce84406cf212f6c02044ba4720d8fe
bulnabang99 <bulnabang99@gmail.com>
2023-03-08 20:52:48 +0900

    test

--- src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceApplication {
-
+    //test
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
     }
